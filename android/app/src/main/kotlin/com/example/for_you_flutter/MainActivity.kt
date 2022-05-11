c18b09c889ce003a2e079023742e3c7c86a5ad8a
Essa Shehab <80629705+EssaShehab99@users.noreply.github.com>
2022-05-11 23:49:10 +0300

    fingerprint done

--- android/app/src/main/kotlin/com/example/for_you_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/for_you_flutter/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.for_you_flutter
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
